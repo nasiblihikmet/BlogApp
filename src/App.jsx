60160a4333f77d1e1760132826b134bb0025325d
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-03 19:56:58 +0400

    created pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
-import HomePage from './pages/home'
+import { Suspense, lazy } from "react";
 
+import { Route, Routes } from "react-router-dom";
+import { Spinner } from "@chakra-ui/react";
+
+
+const HomePage = lazy(()=>import("./pages/home"))
 
 function App() {
-  
-
   return (
-    <>
-     <HomePage/>
-      </>
-  )
+    <Suspense
+      fallback={
+        <Spinner
+          thickness="4px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          size="xl"
+        />
+      }
+    >
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<HomePage />} />
+        <Route path="/faq" element={<HomePage />} />
+        <Route path="/favourites" element={<HomePage />} />
+        <Route path="/article" element={<HomePage />} />
+        <Route path="/create-article" element={<HomePage />} />
+        <Route path="/article/:id" element={<HomePage />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </Suspense>
+  );
 }
 
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import * as React from 'react'
-import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import * as ReactDOM from 'react-dom/client'
-import "./styles/global.css"
+import * as React from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import * as ReactDOM from "react-dom/client";
+import "./styles/global.css";
 import App from "./App.jsx";
-
+import { BrowserRouter } from "react-router-dom";
 
 const colors = {
   brand: {
-    900: '#1a365d',
-    800: '#153e75',
-    700: '#2a69ac',
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
   },
-}
+};
 
-const theme = extendTheme({ colors })
+const theme = extendTheme({ colors });
 
-const rootElement = document.getElementById('root')
+const rootElement = document.getElementById("root");
 ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <ChakraProvider theme={theme}>
+  <ChakraProvider theme={theme}>
+    <BrowserRouter>
       <App />
-    </ChakraProvider>
-  </React.StrictMode>,
-)
+    </BrowserRouter>
+  </ChakraProvider>
+);
