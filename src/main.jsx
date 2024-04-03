0f6e388ab4424c2ec260988f5812930497985e01
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-03 19:09:41 +0400

    add home page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,24 @@
 import * as React from 'react'
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import * as ReactDOM from 'react-dom/client'
 import "./styles/global.css"
 import App from "./App.jsx";
 
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
+
 const rootElement = document.getElementById('root')
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Box, Text } from "@chakra-ui/react"
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <div>
+
+    // adding linear gradient and color transitions
+    <Box w="100%" h="200px" bgGradient="linear(to-t, green.200, pink.500)" />
+    
+    // adding radial gradient and color transitions
+    <Box w="100%" h="200px" bgGradient="radial(gray.300, yellow.400, pink.200)" />
+    
+    // adding the text gradient
+    <Text
+      bgGradient="linear(to-l, #7928CA, #FF0080)"
+      bgClip="text"
+      fontSize="6xl"
+      fontWeight="extrabold"
+    >
+      Welcome to Chakra UI
+    </Text></div>
   )
 }
 
