db5622b770405b305ca5ff51d4f4204703156b3a
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-03 19:21:05 +0400

    update chakra UI

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -5,13 +5,9 @@ function HomePage() {
   return (
     <div>
 
-    // adding linear gradient and color transitions
     <Box w="100%" h="200px" bgGradient="linear(to-t, green.200, pink.500)" />
-    
-    // adding radial gradient and color transitions
     <Box w="100%" h="200px" bgGradient="radial(gray.300, yellow.400, pink.200)" />
     
-    // adding the text gradient
     <Text
       bgGradient="linear(to-l, #7928CA, #FF0080)"
       bgClip="text"
