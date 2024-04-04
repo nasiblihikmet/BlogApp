e7deec6dc38f9ca26189d6df571a3bae53863e4a
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-04 15:59:17 +0400

    test

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,37 @@
-import { Box, Button, Stack } from "@chakra-ui/react";
+import { Box, Button, Stack, Text } from "@chakra-ui/react";
 import React from "react";
 
 function Header() {
   return (
-    <Box height="100px" p="12" alignItems="center" backgroundColor="teal">
-      Header
-      <Stack direction='row' spacing={4} align='center'>
-  
-  <Button colorScheme='teal' variant='ghost'>
-    Button
-  </Button>
-  <Button colorScheme='teal' variant='ghost'>
-    Button
-  </Button>
-  <Button colorScheme='teal' variant='ghost'>
-    Button
-  </Button>
-  <Button colorScheme='teal' variant='ghost'>
-    Button
-  </Button>
-  
-</Stack>
+    <Box
+      height="100px"
+      p="12"
+      alignItems="center"
+      display="flex"
+      justifyContent="space-between"
+      backgroundColor="teal"
+    >
+      <Text>Blog</Text>
+    
+      <Stack direction="row" spacing={4} align="center">
+        <Button variant="ghost" color="white">
+         Home
+        </Button>
+        <Button variant="ghost" color="white">
+          Articles
+        </Button>
+        <Button variant="ghost" color="white">
+          About
+        </Button>
+        <Button variant="ghost" color="white">
+          FAQ
+        </Button>
+        <Button variant="ghost" color="white">
+         Create your article
+        </Button>
+      </Stack>
     </Box>
-  ); 
+  );
 }
 
 export default Header;
