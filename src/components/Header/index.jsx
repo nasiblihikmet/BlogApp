95dee9b29a0ab517b0247ce79803d6319564140e
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-04 15:53:20 +0400

    created header menu

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,28 @@
-import { Box } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Button, Stack } from "@chakra-ui/react";
+import React from "react";
 
 function Header() {
   return (
-    <Box backgroundColor="teal">Header</Box>
-  )
+    <Box height="100px" p="12" alignItems="center" backgroundColor="teal">
+      Header
+      <Stack direction='row' spacing={4} align='center'>
+  
+  <Button colorScheme='teal' variant='ghost'>
+    Button
+  </Button>
+  <Button colorScheme='teal' variant='ghost'>
+    Button
+  </Button>
+  <Button colorScheme='teal' variant='ghost'>
+    Button
+  </Button>
+  <Button colorScheme='teal' variant='ghost'>
+    Button
+  </Button>
+  
+</Stack>
+    </Box>
+  ); 
 }
 
-export default Header
+export default Header;
