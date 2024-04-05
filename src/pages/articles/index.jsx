1416066a86f83441e5fafc183595ad2ab6106b96
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-05 21:21:09 +0400

    i used helper instance axios

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Box, Input, SimpleGrid } from "@chakra-ui/react";
 import Header from "../../components/Header";
 import BlogCard from "../../components/BlogCard";
 import NavigationShow from "../../components/NavigationShow";
+import { getBlogs } from "../../services/articles";
 
 function ArticlesPage() {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await getBlogs();
+        setData(res.data);
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    
+    fetchData();
+
+    // (async function(){
+    // })()
+  }, []);
+
   return (
     <>
       <Header />
