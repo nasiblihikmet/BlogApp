47f9b5b092c9adde391209263fb351f266add1f6
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-07 13:48:14 +0400

    created hooks

--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,32 +1,25 @@
 import { useEffect, useState } from "react";
 
+export const useFetchData = ({ requestFn, dependecy = [] }) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState();
 
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const res = await requestFn(); //? everything okay
+        setData(res.data); //? everything okay
+      } catch (err) {
+        setError(err);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-export const useFetchData =({
-    requestFn,
-    dependecy = []
-})=>{
+    fetchData();
+  }, [...dependecy]);
 
-    const [data, setData] = useState();
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
-  
-    useEffect(() => {
-      const fetchData = async () => {
-        setLoading(true);
-        try {
-          const res = await getBlogs(); //? everything okay
-          setData(res.data); //? everything okay
-        } catch (err) {
-          setError(err);
-        } finally {
-          setLoading(false);
-        }
-      };
-  
-      fetchData();
-    }, [...dependecy]);
-
-    return {data,loading,error}
-  
-}
+  return { data, loading, error };
+};
