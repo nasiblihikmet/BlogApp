6706021808cb97df74dd90416eeaf11a2985c1ed
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-08 20:00:44 +0400

    test2

--- src/pages/notfound/index.jsx
+++ src/pages/notfound/index.jsx
@@ -4,12 +4,7 @@ import { Box, Text } from "@chakra-ui/react";
 function NotFound() {
   return (
     <div>
-      {/* <Box w="100%" h="200px" bgGradient="linear(to-t, green.200, pink.500)" />
-      <Box
-        w="100%"
-        h="200px"
-        bgGradient="radial(gray.300, yellow.400, pink.200)"
-      /> */}
+   
 
       <Text
         bgGradient="linear(to-l, #7928CA, #FF0080)"
