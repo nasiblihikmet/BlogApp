2936aeb262412e0e04adc2ee8424a731bbe11276
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-11 15:05:58 +0400

    test loading

--- src/components/Loading/index.jsx
+++ src/components/Loading/index.jsx
@@ -1,12 +1,12 @@
-import { Box, Spinner } from '@chakra-ui/react'
-import React from 'react'
+import { Box, Spinner } from "@chakra-ui/react";
+import React from "react";
 
 function Loading() {
   return (
     <Box display="flex" justifyContent="center" p={50}>
-          <Spinner size="lg" />
-        </Box>
-  )
+      <Spinner size="lg" />
+    </Box>
+  );
 }
 
-export default Loading
+export default Loading;
