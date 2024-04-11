d58c234e7a60653eb512881e725e09200de4f5b0
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-11 15:18:44 +0400

    update about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,20 +1,44 @@
-import React from "react";
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Image, Text } from "@chakra-ui/react";
+import Header from "../../components/Header";
+import { useTitle } from "../../hooks/useTitle";
 
 function AboutPage() {
-  return (
-    <div>
-     
+  useTitle("About | Blog app");
 
-      <Text
-        bgGradient="linear(to-l, #7928CA, #FF0080)"
-        bgClip="text"
-        fontSize="6xl"
-        fontWeight="extrabold"
-      >
-       About
-      </Text>
-    </div>
+  return (
+    <>
+      <Header />
+      <Box p={50}>
+        <Text fontSize="6xl" align="center">
+          About us
+        </Text>
+        <Text fontSize="lg" align="center">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.
+          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          cumque velit quibusdam sed amet tempora. Sit laborum ab, eius fugit
+          doloribus tenetur fugiat, temporibus enim commodi iusto libero magni
+          deleniti quod quam consequuntur! Commodi minima excepturi repudiandae
+          velit hic maxime doloremque. Quaerat provident commodi consectetur
+          veniam similique ad earum omnis ipsum saepe, voluptas, hic voluptates
+          pariatur est explicabo fugiat, dolorum eligendi quam cupiditate
+          excepturi mollitia maiores labore suscipit quas? Nulla, placeat.
+          Voluptatem quaerat non architecto ab laudantium modi minima sunt esse
+          temporibus sint culpa, recusandae aliquam numquam totam ratione
+          voluptas quod exercitationem fuga. Possimus quis earum veniam quasi
+          aliquam eligendi, placeat qui corporis!
+        </Text>
+      </Box>
+    </>
   );
 }
 
