2c910ad1f2bcc22bd54064fd9e436d687511ca74
nasiblihikmet <hikmetnesibli147@gmail.com>
2024-04-11 15:17:27 +0400

    created new hook useTitle

--- src/helpers/instanceAxios.ts
+++ src/helpers/instanceAxios.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const instanceAxios = axios.create({
-    baseURL: "https://blog-api-t6u0.onrender.com/",
+  baseURL: "https://blog-api-t6u0.onrender.com/",
 });
--- src/hooks/useFetchData.js
+++ src/hooks/useFetchData.js
@@ -1,27 +1,36 @@
 import { useEffect, useState } from "react";
 
-export const useFetchData = ({ requestFn, dependecy = [] }) => {
+export const useFetchData = ({
+  requestFn,
+  dependecy = [],
+  condition,
+  onSuccess,
+  onError,
+}) => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState();
 
   useEffect(() => {
+    if (condition) return;
+
     const fetchData = async () => {
       setLoading(true);
       try {
-        const res = await requestFn(); //? everything okay
-        setData(res.data); //? everything okay
+        const res = await requestFn();
+        setData(res.data);
+        onSuccess?.(res.data);
       } catch (err) {
         setError(err);
+        onError?.(err);
       } finally {
         setLoading(false);
       }
     };
-
     fetchData();
   }, [...dependecy]);
 
-  return { data, loading, error };
+  return { data, loading, error, setData };
 };
 
 
